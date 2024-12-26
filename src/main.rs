c5be0b0fd36ddf8a706fe15d59680d48ea0a8a62
Lukasz Kielar <31779738+lukaszKielar@users.noreply.github.com>
2024-12-26 16:42:08 +1100

    simple webserver

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,72 @@
-use std::marker::PhantomData;
-
+use anyhow::Result;
 use esp_idf_svc::{
-    hal::{
-        delay::FreeRtos,
-        gpio::{self, Pin},
-        prelude::Peripherals,
-        units::{FromValueType, Hertz},
+    eventloop::EspSystemEventLoop,
+    hal::{delay::FreeRtos, prelude::Peripherals, units::FromValueType},
+    http::{self},
+    io::{EspIOError, Write as _},
+    sys::c_camera::{
+        camera_fb_location_t_CAMERA_FB_IN_PSRAM, camera_grab_mode_t_CAMERA_GRAB_WHEN_EMPTY,
+        framesize_t_FRAMESIZE_UXGA, ledc_channel_t_LEDC_CHANNEL_0, ledc_timer_t_LEDC_TIMER_0,
+        pixformat_t_PIXFORMAT_JPEG,
     },
-    sys::{
-        camera::{
-            self, camera_fb_location_t_CAMERA_FB_IN_PSRAM,
-            camera_grab_mode_t_CAMERA_GRAB_WHEN_EMPTY, framesize_t_FRAMESIZE_QVGA,
-            ledc_channel_t_LEDC_CHANNEL_0, ledc_timer_t_LEDC_TIMER_0, pixformat_t_PIXFORMAT_JPEG,
-        },
-        esp, gpio_num_t_GPIO_NUM_NC, EspError,
-    },
+    wifi::{self},
 };
-use log::{error, info};
+use log::{info, warn};
+use rusty_esp32_cam::Camera;
 
-struct Camera<'a> {
-    _p: PhantomData<&'a ()>,
+#[toml_cfg::toml_config]
+pub struct Config {
+    #[default("")]
+    wifi_ssid: &'static str,
+    #[default("")]
+    wifi_psk: &'static str,
 }
 
-impl<'a> Camera<'a> {
-    pub fn new(
-        pin_pwdn: gpio::Gpio32,
-        pin_xclk: gpio::Gpio0,
-        pin_d7: gpio::Gpio35,
-        pin_d6: gpio::Gpio34,
-        pin_d5: gpio::Gpio39,
-        pin_d4: gpio::Gpio36,
-        pin_d3: gpio::Gpio21,
-        pin_d2: gpio::Gpio19,
-        pin_d1: gpio::Gpio18,
-        pin_d0: gpio::Gpio5,
-        pin_vsync: gpio::Gpio25,
-        pin_href: gpio::Gpio23,
-        pin_pclk: gpio::Gpio22,
-        pin_sda: gpio::Gpio26,
-        pin_scl: gpio::Gpio27,
-        xclk_freq_hz: Hertz,
-        ledc_timer: camera::ledc_timer_t,
-        ledc_channel: camera::ledc_channel_t,
-        pixel_format: camera::pixformat_t,
-        frame_size: camera::framesize_t,
-        jpeg_quality: i32,
-        fb_count: usize,
-        fb_location: camera::camera_fb_location_t,
-        grab_mode: camera::camera_grab_mode_t,
-    ) -> Result<Self, EspError> {
-        let cam_config = camera::camera_config_t {
-            pin_pwdn: pin_pwdn.pin(),
-            pin_reset: gpio_num_t_GPIO_NUM_NC,
-            pin_xclk: pin_xclk.pin(),
-
-            pin_d7: pin_d7.pin(),
-            pin_d6: pin_d6.pin(),
-            pin_d5: pin_d5.pin(),
-            pin_d4: pin_d4.pin(),
-            pin_d3: pin_d3.pin(),
-            pin_d2: pin_d2.pin(),
-            pin_d1: pin_d1.pin(),
-            pin_d0: pin_d0.pin(),
-            pin_vsync: pin_vsync.pin(),
-            pin_href: pin_href.pin(),
-            pin_pclk: pin_pclk.pin(),
-
-            xclk_freq_hz: xclk_freq_hz.0 as i32,
-            ledc_timer,
-            ledc_channel,
-
-            pixel_format,
-            frame_size,
-
-            jpeg_quality,
-            fb_count,
-            fb_location,
-            grab_mode,
-
-            __bindgen_anon_1: camera::camera_config_t__bindgen_ty_1 {
-                pin_sccb_sda: pin_sda.pin(),
-            },
-            __bindgen_anon_2: camera::camera_config_t__bindgen_ty_2 {
-                pin_sccb_scl: pin_scl.pin(),
-            },
-
-            sccb_i2c_port: -1,
-        };
-
-        esp!(unsafe { camera::esp_camera_init(&cam_config) })?;
-
-        Ok(Self { _p: PhantomData })
-    }
-}
-
-impl<'a> Drop for Camera<'a> {
-    fn drop(&mut self) {
-        info!("Dropping Camera");
-        if esp!(unsafe { camera::esp_camera_deinit() }).is_err() {
-            error!("Error dropping Camera!");
-        }
-    }
-}
-
-fn main() {
+fn main() -> Result<()> {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
     let peripherals = Peripherals::take().unwrap();
+    let sysloop = EspSystemEventLoop::take()?;
 
+    let wifi = wifi::EspWifi::new(peripherals.modem, sysloop.clone(), None)?;
+    let mut wifi = wifi::BlockingWifi::wrap(wifi, sysloop)?;
+
+    wifi.set_configuration(&wifi::Configuration::Client(
+        wifi::ClientConfiguration::default(),
+    ))?;
+
+    info!("Starting WiFi...");
+    wifi.start()?;
+
+    info!("Scanning...");
+    let ap_infos = wifi.scan()?;
+    let channel = if let Some(ap) = ap_infos.into_iter().find(|ap| ap.ssid == CONFIG.wifi_ssid) {
+        info!("Access point found on channel {:?}", ap.channel);
+        Some(ap.channel)
+    } else {
+        warn!("Cannot find access point");
+        None
+    };
+
+    wifi.set_configuration(&wifi::Configuration::Client(wifi::ClientConfiguration {
+        ssid: CONFIG.wifi_ssid.try_into().unwrap(),
+        password: CONFIG.wifi_psk.try_into().unwrap(),
+        auth_method: wifi::AuthMethod::WPA2WPA3Personal,
+        channel,
+        ..Default::default()
+    }))?;
+
+    info!("Connecting to WiFi...");
+    wifi.connect()?;
+
+    info!("Waiting for DHCP lease...");
+    wifi.wait_netif_up()?;
+
+    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+    info!("Wifi DHCP info: {:?}", ip_info);
+
+    info!("Creating camera instance...");
     let camera = Camera::new(
         peripherals.pins.gpio32,
         peripherals.pins.gpio0,
@@ -129,18 +87,38 @@ fn main() {
         ledc_timer_t_LEDC_TIMER_0,
         ledc_channel_t_LEDC_CHANNEL_0,
         pixformat_t_PIXFORMAT_JPEG,
-        framesize_t_FRAMESIZE_QVGA,
-        20,
+        framesize_t_FRAMESIZE_UXGA, // QVGA || XGA || UXGA
+        10,
         1,
         camera_fb_location_t_CAMERA_FB_IN_PSRAM,
         camera_grab_mode_t_CAMERA_GRAB_WHEN_EMPTY,
-    )
-    .unwrap();
-    info!("Camera instance created");
+    )?;
 
-    drop(camera);
+    let mut server = http::server::EspHttpServer::new(&http::server::Configuration::default())?;
+
+    server.fn_handler("/", http::Method::Get, move |request| {
+        camera.get_framebuffer();
+        // take two frames to get a fresh one
+        let framebuffer = camera.get_framebuffer();
+
+        if let Some(framebuffer) = framebuffer {
+            let data = framebuffer.data();
+
+            let headers = [
+                ("Content-Type", "image/jpeg"),
+                ("Content-Length", &data.len().to_string()),
+            ];
+            let mut response = request.into_response(200, Some("OK"), &headers)?;
+            response.write_all(data)?;
+        } else {
+            let mut response = request.into_ok_response()?;
+            response.write_all("no framebuffer".as_bytes())?;
+        }
+
+        Ok::<(), EspIOError>(())
+    })?;
 
     loop {
-        FreeRtos::delay_ms(500u32);
+        FreeRtos::delay_ms(1_000u32);
     }
 }
